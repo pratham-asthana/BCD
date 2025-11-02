b150520c8908b5f177c802385948b545a55d487b
Pratham Asthana <prathamasthana.cse22@jimsgn.org>
2025-11-02 21:38:51 +0530

    commit

--- BREAK-HIS/dataset_cancer_v1/BREAK-HIS-dataset_binary/Combined_Dataset/dataset_splitter.py
+++ BREAK-HIS/dataset_cancer_v1/BREAK-HIS-dataset_binary/Combined_Dataset/dataset_splitter.py
@@ -3,9 +3,9 @@ import shutil
 import random
 from pathlib import Path
 
-data_dir = Path("BREAK-HIS\\dataset_cancer_v1\\BREAK-HIS-dataset_binary\\Combined_Dataset\\100X_BALANCED")
+data_dir = Path("BREAK-HIS\\dataset_cancer_v1\\BREAK-HIS-dataset_binary\\Combined_Dataset\\200X_BALANCED")
 
-base_dir = Path("Final-100X")
+base_dir = Path("Final-200X")
 train_dir = base_dir / "train"
 test_dir = base_dir / "test"
 val_dir = base_dir / "val"
